ab2fdb739cd143348f809ca0de2417beade094fe
bran.kuang <bran.kuang@STKWX32519>
2017-02-08 14:31:40 +0800

    Add project files.

---
+++ winform/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="winform.MvcApplication" Language="C#" %>
---
+++ winform/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(winform.Startup))]
+namespace winform
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
